1769301fb622e81a7cff5d81697718de17e518b0
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-18 18:03:41 -0500

    mongoskin initial setup via localhost. also attach a collection to request via app.params

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ var levenshtein = require('fast-levenshtein');
 
 var app = express();
 
+var db = mongoskin.db("mongodb://@localhost:27017/nutritionixdb", {safe: true});
+// helper for searching by ID
+var id = mongoskin.helper.toObjectID;
+
 var allowMethods = function(req, res, next) {
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 }
+
+// define behavior of params when receieved via URL e.g. /api/:collection/:id
+app.params(":collection", function(req, res, next, collection) {
+	req.collection= db.collection(collection);
+});
 
 app.listen(APP_PORT, function() {
 	console.log("Express listening on port " + APP_PORT);
