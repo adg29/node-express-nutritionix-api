777a21c37b63752ea7dbade35f8a0178e4851c5b
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-18 20:04:29 -0500

    Correctly define behavior of collection param. Correctly pass through allowMethods by invoking next. Update dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const APP_PORT = 8081;
 
 var express = require("express");
 var mongoskin = require("mongoskin");
-var bodyParse = require("body-parser");
+var bodyParser = require("body-parser");
 var reqPromise = require('request-promise')  
 var stopword = require("stopword");
 var levenshtein = require('fast-levenshtein');
@@ -16,10 +16,11 @@ var id = mongoskin.helper.toObjectID;
 
 var allowMethods = function(req, res, next) {
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	next();
 }
 
-// define behavior of params when receieved via URL e.g. /api/:collection/:id
-app.params(":collection", function(req, res, next, collection) {
+// define behavior of param when receieved via URL e.g. /api/:collection/:id
+app.param(":collection", function(req, res, next, collection) {
 	req.collection= db.collection(collection);
 });
 
